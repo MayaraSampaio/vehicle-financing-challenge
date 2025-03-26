d2909b589bfad9b2f927facd4e0e55ab9bf70c76
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2025-03-26 11:02:46 +0000

    Create POST guardar in SimuladorFinanciamentoController and guardarFinanciamento in SimuladorFinanciamentoService

--- src/main/java/com/desafio/financiamento/controllers/SimuladorFinanciamentoController.java
+++ src/main/java/com/desafio/financiamento/controllers/SimuladorFinanciamentoController.java
@@ -1,5 +1,6 @@
 package com.desafio.financiamento.controllers;
 import com.desafio.financiamento.dto.SimuladorFinanciamentoCalculaDTO;
+import com.desafio.financiamento.dto.SimuladorFinanciamentoGuardaDTO;
 import com.desafio.financiamento.entities.Enums.TipoFinanciamento;
 import com.desafio.financiamento.service.SimuladorFinanciamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 
 @RestController
@@ -25,5 +28,11 @@ public class SimuladorFinanciamentoController {
         dto.setValorPrestacao(valorPrestacao);
         return ResponseEntity.ok(dto);
     }
+    @PostMapping("/guardar")
+    public ResponseEntity<String> guardarFinanciamento(@RequestBody SimuladorFinanciamentoGuardaDTO salvarDto) throws IOException {
+        service.guardarFinanciamento(salvarDto);
+        return ResponseEntity.ok("Simulação de financiamento salva com sucesso");
+    }
+
 
 }
--- src/main/java/com/desafio/financiamento/service/SimuladorFinanciamentoService.java
+++ src/main/java/com/desafio/financiamento/service/SimuladorFinanciamentoService.java
@@ -1,8 +1,12 @@
 package com.desafio.financiamento.service;
 
+import com.desafio.financiamento.dto.SimuladorFinanciamentoGuardaDTO;
 import com.desafio.financiamento.entities.Enums.TipoFinanciamento;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 import static com.desafio.financiamento.entities.Enums.TipoFinanciamento.EXTERNO;
 import static com.desafio.financiamento.entities.Enums.TipoFinanciamento.INTERNO;
@@ -15,6 +19,7 @@ public class SimuladorFinanciamentoService {
     @Value("${properties.fator.externo:1.065}")
     private Double fatorExterno;
 
+    private static final String CAMINHO_ARQUIVO = "dados_financiamento.txt";
 
     public Double calculaFinanciamento(TipoFinanciamento tipoFinanciamento, Integer numeroMensalidades, Double valorViatura) {
         if (tipoFinanciamento == EXTERNO && numeroMensalidades >= 12 && numeroMensalidades <= 60) {
@@ -26,6 +31,20 @@ public class SimuladorFinanciamentoService {
         throw new IllegalArgumentException("Parametros de financiamento inválidos");
     }
 
+    public void guardarFinanciamento(SimuladorFinanciamentoGuardaDTO guardaDTO){
+        String dados = String.format("nome: %s\nContato: %s\nTipo de Financiamento: %s\nNº de Mensalidades: %d\n" +
+                        "Valor da Viatura: %.2f\nPrestação Mensal: %2f\n-------------------------------------------------\n",
+                guardaDTO.getCliente().getNome(),guardaDTO.getCliente().getContacto(),guardaDTO.getTipoFinanciamento(),
+                guardaDTO.getNumeroMensalidades(),guardaDTO.getValorViatura(),guardaDTO.getValorPrestacao());
+
+        try(FileWriter fileWriter = new FileWriter(CAMINHO_ARQUIVO,true)){
+            fileWriter.write(dados);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao salvar financiamento no arquivo: " + CAMINHO_ARQUIVO,e);
+        }
+
+    }
     public double calculaPrestacaoMensal(Double fator, Double valorDaViatura, Integer numeroDeMensalidades){
         double valorPrestacao = valorDaViatura * fator/numeroDeMensalidades;
 
