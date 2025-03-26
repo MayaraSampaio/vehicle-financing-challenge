553ea8eab8873c013cdedcf2ea6ec2dcea4a1acc
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2025-03-26 22:07:19 +0000

    add logs in controller

--- src/main/java/com/desafio/financiamento/controllers/SimuladorFinanciamentoController.java
+++ src/main/java/com/desafio/financiamento/controllers/SimuladorFinanciamentoController.java
@@ -3,16 +3,14 @@ import com.desafio.financiamento.dto.SimuladorFinanciamentoCalculaDTO;
 import com.desafio.financiamento.dto.SimuladorFinanciamentoGuardaDTO;
 import com.desafio.financiamento.entities.Enums.TipoFinanciamento;
 import com.desafio.financiamento.service.SimuladorFinanciamentoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
-
+@Slf4j
+@CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping(value = "/simulacao")
 public class SimuladorFinanciamentoController {
@@ -26,13 +24,13 @@ public class SimuladorFinanciamentoController {
                 dto.getNumeroMensalidades(),dto.getValorViatura());
 
         dto.setValorPrestacao(valorPrestacao);
+        log.info("Simulação de financiamento calculada com sucesso " + dto);
         return ResponseEntity.ok(dto);
     }
     @PostMapping("/guardar")
-    public ResponseEntity<String> guardarFinanciamento(@RequestBody SimuladorFinanciamentoGuardaDTO salvarDto) throws IOException {
-        service.guardarFinanciamento(salvarDto);
+    public ResponseEntity<String> guardaFinanciamento(@RequestBody SimuladorFinanciamentoGuardaDTO salvarDto) throws IOException {
+        service.guardaFinanciamento(salvarDto);
+        log.info("Simulação de financiamento salva com sucesso " + salvarDto);
         return ResponseEntity.ok("Simulação de financiamento salva com sucesso");
     }
-
-
 }
